c1e34fb51bbcf805a93b748017dc9745e27b1d6b
hellollw <hiluliwen@gmail.com>
2019-11-21 11:01:00 +0800

    0.0

--- Dimension_Reduction/Laplacian Eigenmap/Laplacian_Eigenmap.py
+++ Dimension_Reduction/Laplacian Eigenmap/Laplacian_Eigenmap.py
@@ -14,9 +14,15 @@ Laplacian Eigenmap(特征映射降维）
 """
 
 from numpy import *
-
+from lib import GraphCalculate as Graph
 
 # knn建图
 # 输入：样本数据矩阵:X, 选用的边值权重确定方式:kTup
 # 输出：稀疏矩阵sparse_matrix
-def knnConstruct(X,KTup):
+def knnConstruct(X,kTup):
+    sparse_matrix = Graph.constructDist(X,kTup)
+    return sparse_matrix
+
+# 构造拉普拉斯矩阵
+# 输入：稀疏矩阵sparse_matrix
+# 返回：拉普拉斯矩阵:laplacian_mat,度矩阵:D,邻接矩阵：A
