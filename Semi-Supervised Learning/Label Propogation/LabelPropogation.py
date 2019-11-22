249f731f47bfeaa2a2e21986e99985e2fefbbbb6
hellollw <hiluliwen@gmail.com>
2019-11-22 16:00:58 +0800

    Label Propogation

--- Semi-Supervised Learning/Label Propogation/LabelPropogation.py
+++ Semi-Supervised Learning/Label Propogation/LabelPropogation.py
@@ -6,7 +6,117 @@
 LabelPropogation(标签传播算法）
 可用少量的标注数据得到大量无标注数据的标签值
 
+使用iris数据集，用测试样本作为标签值，用训练样本作为无标签值
+
+1.半监督学习的数据存放方式(有标签样本放在上面）：
+    标签数据————标签label
+        +
+    无标签数据
+        =
+    样本数据集和datamat
 """
 
-import numpy as *
+from numpy import *
+from lib import BasicFunction as Basic
+from lib import GraphCalculate as Graph
 
+
+# 根据样本点构造k近邻图
+# 输入：样本数据:datamat, 近邻选取数量:k
+# 输出: k近邻关系
+def constructKmat(datamat, k):
+    kTup = ('knn', 'dist', 0, k)
+    kmat = Graph.constructKmat(datamat, kTup)
+    return kmat
+
+
+# 构建权值矩阵W
+# 输入：近邻图:kmat, 权重计算方式:kTup
+# 输出：权值矩阵W
+def constructWmat(kmat, kTup):
+    if kTup[0] == 'rbf':  # 选用rbf内核
+        W = exp(-kmat / kTup[1] ** 2)
+    else:
+        raise NameError('还没定义' + kTup[0] + '这种方法')
+    return W
+
+
+# 构建传播概率矩阵
+# 输入:权值矩阵：W
+# 输出：传播概率矩阵：T
+def constructT(W):
+    # T = mat(zeros(shape(W)))
+    col_sum = sum(W, 0)  # 按行缩减
+    T = W / col_sum
+    return T
+
+
+# 进入标签传播循环
+# 输入：传播概率矩阵T, 有标签数据标签列表:label, 最大循环次数:maxiter
+# 输出：无标签数据的标签矩阵:Yuu_label
+def propogation(T, label, maxiter):
+    m_label = len(label)
+    m_unlabel = shape(T)[0] - m_label
+    Tul = T[m_label:m_label + m_unlabel, 0:m_label].copy()
+    Tuu = T[m_label:m_label + m_unlabel, m_label:m_label + m_unlabel].copy()
+    # 找到标签总个数
+    Whole_labellist = []
+    for label_index in range(m_label):
+        if label[label_index] not in Whole_labellist:
+            Whole_labellist.append(label[label_index])
+        else:
+            continue
+    label_num = len(Whole_labellist)
+    # 构建标签矩阵
+    Yll = mat(zeros((m_label, label_num)))
+    for i in range(m_label):
+        collabel = Whole_labellist.index(label[i])  # 找出标签所在的列记号
+        Yll[i, collabel] = 1
+    Yuu = mat(ones((m_unlabel, label_num)))
+    # 进入循环
+    iter = 0
+    while iter < maxiter:
+        Yuu = Tul * Yll + Tuu * Yuu
+        print('现在为第%d轮循环'%iter)
+        iter += 1
+    Yuu_label_list = argmax(Yuu, 1).T.A[0]  # 以每行的最大值索引作为标签值
+    Yuu_label = []
+    for j in Yuu_label_list:
+        Yuu_label.append(Whole_labellist[j])
+    return Yuu_label
+
+
+# 标签传播算法
+# 输入：有标签样本数据:label_mat,无标签样本数据:unlabel_mat,标签集和:label,权值计算方式：kTup,近邻选取数量:k,最大迭代次数:maxiter
+# 输出：无标签样本数据：unlabel_list
+def labelPropogation(label_mat, unlabel_mat, label, kTup, k, maxiter):
+    # 构造数据集
+    ml, nl = shape(label_mat)
+    mu, nu = shape(unlabel_mat)
+    if nl != nu:
+        raise SystemError('样本维度不一致')
+    else:
+        datamat = mat(zeros((ml + mu, nu)))
+        datamat[0:ml, :] = label_mat.copy()
+        datamat[ml:ml + mu, :] = unlabel_mat.copy()
+        kmat = constructKmat(datamat, k)
+        Wmat = constructWmat(kmat, kTup)
+        T = constructT(Wmat)
+        unlabel_list = propogation(T, label, maxiter)
+        return unlabel_list
+
+
+if __name__ == '__main__':
+    path = './test/iris.txt'
+    training_data, training_label, test_data, test_label = Basic.readData(path)
+    kTup = ('rbf',3)
+    k = 50
+    maxiter = 80
+    unlabel_list = labelPropogation(test_data,training_data,test_label,kTup,k,maxiter)
+    wrong = 0
+    for w in range(len(unlabel_list)):
+        if unlabel_list[w]!=training_label[w]:
+            wrong+=1
+        else:
+            continue
+    print(wrong)
---
+++ Semi-Supervised Learning/Label Propogation/lib/GraphCalculate.py
@@ -0,0 +1,79 @@
+# -*- coding:utf-8 -*-
+# Author: Lu Liwen
+# Modified Time: 2019-11-21
+
+"""
+图论计算公式：
+    不同核方式计算图的边界权值
+
+计算k近邻
+"""
+
+from numpy import *
+
+# 定义核函数计算公式
+# 输入：m个样本数据：datam, 一个想要进入核函数矩阵的样本数据i:datai, 描述核函数的元组:kTup
+# 输出：对于i样本在m个样本数据中的核矩阵:dataK
+def kernelCal(datam, datai, kTup):
+    """
+    dist返回距离的平方
+    """
+    m, n = shape(datam)
+    datai = mat(datai).transpose()  # 转换为列向量,transpose()默认与.T相同
+    dataK = mat(zeros((m, 1)))
+    if kTup[0] == 'lin':  # 该线性内核指的是向量内积运算
+        dataK = datam * datai
+    elif kTup[0] == 'rbf':
+        sigma = kTup[1]
+        for j in range(m):
+            dataj = datam[j, :].transpose()
+            dif = dataj - datai
+            dataK[j, :] = dif.T * dif
+        dataK = exp(dataK / (-2 * sigma ** 2))  # 元素除法
+    elif kTup[0] == 'dist':  # 距离核
+        for j in range(m):
+            dataj = datam[j, :].T
+            dif = dataj - datai
+            dif = dif.T * dif
+            dataK[j, 0] = sum(dif)
+    else:
+        raise NameError('That kernel is not defined')
+    return dataK
+
+# 选取k近邻构建稀疏矩阵
+# 输入：输入样本数据：datamat, 选择近邻构建方式:kTup
+# 输出：稀疏矩阵:kmat(若边之间没有相连则取值为0）
+def constructKmat(datamat, kTup):
+    curkTup = kTup[1:]
+    m = shape(datamat)[0]
+    kmat = mat(zeros((m, m)))+inf
+    for i in range(m):
+        samplei = datamat[i, :]  # 取出样本
+        dataKernal = kernelCal(datam=datamat, datai=samplei, kTup=curkTup)
+        sampleindex = dataKernal.T.A[0].argsort()  # 先转换为一维array，再返回从小到大的索引
+        for j in range(curkTup[2]):  # 取前k个近邻,构建稀疏矩阵（为对称矩阵）
+            kmat[sampleindex[j], i] = dataKernal[sampleindex[j], 0]
+            kmat[i, sampleindex[j]] = dataKernal[sampleindex[j], 0]  # 满足图的对称性
+    return kmat
+
+# 构造距离矩阵dist(使用不同的内核取构造距离矩阵），经典MDS输入的距离矩阵应该为欧式矩阵
+# 输入：输入样本数据：datamat, 核方式:kTup
+# 输出：距离矩阵dist
+def constructDist(datamat, kTup):
+    """
+
+    :param kTup:可选形式：
+    1.rbf内核（'rbf',超参数:sigma=)
+    2.向量内积内核('lin',)
+    3.距离内核('dist',)
+    4.k近邻选取('knn',边界计算方式: , 计算方式参数: , k近邻居数量: )
+    :return: 图的距离表示矩阵
+    """
+    m = shape(datamat)[0]
+    dist = mat(zeros((m, m)))
+    if kTup[0] == 'knn':  # knn近邻选取
+        dist = constructKmat(datamat, kTup)
+    else:   #其余的都在核函数函数中
+        for i in range(m):
+            dist[:, i] = kernelCal(datamat, datamat[i, :], kTup)
+    return dist
